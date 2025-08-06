41a362e46386835c9c2394a4b1af36b96f7a80d9
Anthony de Cuyper <decuyperanthony@gmail.com>
2025-08-06 09:29:06 +0200

    fix: duplicate and incorrect IDs in defaultPrelevements
    Corrected duplicate IDs and adjusted the amount for one SFR entry in the defaultPrelevements array. This ensures each prelevement has a unique ID and accurate data.

--- app/components/prelevement.tsx
+++ app/components/prelevement.tsx
@@ -16,29 +16,29 @@ interface Prelevement {
 }
 
 const defaultPrelevements: Prelevement[] = [
-  { id: 1, title: "SFR", day: 2, amount: 33.99, completed: false },
-  { id: 2, title: "SFR", day: 4, amount: 1.99, completed: false },
-  { id: 3, title: "Canal +", day: 4, amount: 45.99, completed: false },
-  { id: 4, title: "SFR", day: 5, amount: 37.85, completed: false },
-  { id: 5, title: "CAF", day: 5, amount: -75.53, completed: false },
-  { id: 6, title: "Spotify", day: 5, amount: 17.2, completed: false },
-  { id: 6, title: "Amazon Prime", day: 5, amount: 6.99, completed: false },
-  { id: 7, title: "Scooter", day: 5, amount: 166.59, completed: false },
-  { id: 8, title: "SFR", day: 5, amount: 9.99, completed: false },
-  { id: 9, title: "Climatiseur", day: 6, amount: 69.9, completed: false },
-  { id: 10, title: "L'equipe", day: 6, amount: 3.99, completed: false },
-  { id: 11, title: "Apple 2TO", day: 10, amount: 9.99, completed: false },
-  { id: 12, title: "Loyer", day: 10, amount: 1100.0, completed: false },
-  { id: 13, title: "MAIF", day: 10, amount: 94.03, completed: false },
+  { id: 1, title: "SFR", day: 2, amount: 33.99, completed: false }, //
+  { id: 2, title: "SFR", day: 4, amount: 1.99, completed: false }, //
+  { id: 3, title: "Canal +", day: 4, amount: 45.99, completed: false }, //
+  { id: 4, title: "SFR", day: 5, amount: 37.85, completed: false }, //
+  { id: 5, title: "CAF", day: 5, amount: -75.53, completed: false }, //
+  { id: 14, title: "Spotify", day: 5, amount: 17.2, completed: false }, //
+  { id: 6, title: "Amazon Prime", day: 5, amount: 6.99, completed: false }, //
+  { id: 7, title: "Scooter", day: 5, amount: 166.59, completed: false }, //
+  { id: 8, title: "SFR", day: 5, amount: 8.99, completed: false }, //
+  { id: 9, title: "Climatiseur", day: 6, amount: 69.9, completed: false }, //
+  { id: 10, title: "L'equipe", day: 6, amount: 3.99, completed: false }, //
+  { id: 11, title: "Apple 2TO", day: 10, amount: 9.99, completed: false }, //
+  { id: 12, title: "Loyer", day: 10, amount: 1100.0, completed: false }, //
+  { id: 13, title: "MAIF", day: 10, amount: 94.03, completed: false }, //
   {
     id: 14,
     title: "Science et vie junior",
     day: 12,
     amount: 5.44,
     completed: false,
-  },
-  { id: 15, title: "EDF", day: 16, amount: 135.0, completed: false },
-  { id: 16, title: "Impot", day: 16, amount: 38.0, completed: false },
+  }, //
+  { id: 15, title: "EDF", day: 16, amount: 135.0, completed: false }, //
+  { id: 16, title: "Impot", day: 16, amount: 38.0, completed: false }, //
 ];
 
 export default function Prelevement() {
